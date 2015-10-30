9314631e4376fd40a8d203b537b013e10fc90735
Nalam V S S Krishna Chaitanya <cs13b021@smail.iitm.ac.in>
2015-10-30 23:57:34 +0530

    Two new graphs are added by varying test script.

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 make
 ./data 100 1000
-for f in {1..100}
+for f in {1..600}
 do
-	for r in {1..3}
+	for r in {1..1}
 	do
 		./matmul $f mat.dat >> repitition.txt
 	done
 	echo "$f"
 done
-./graph repitition.txt 3 100 result.txt
+./graph repitition.txt 1 600 result.txt
 echo "set terminal 'png'" >> plot.txt
-echo "set output 'graph.png'" >> plot.txt
+echo "set output 'graph2.png'" >> plot.txt
 echo "plot 'result.txt' with linespoints" >> plot.txt
 gnuplot < plot.txt
 rm plot.txt
